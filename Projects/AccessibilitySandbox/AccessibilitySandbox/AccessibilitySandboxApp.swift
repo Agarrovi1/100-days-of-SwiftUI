5a675288a3d1d90180ed8d18161ce8161eb9d7fe
Agarrovi1 <52180618+Agarrovi1@users.noreply.github.com>
2025-06-02 12:10:59 -0400

    add AccessibilitySandbox project

---
+++ Projects/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Angela Garrovillas on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
